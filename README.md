82afe644f7b63f6b3973990580cdeb822df281e9
Bram Boogaard | Pivoton <b.boogaard@pivoton.nl>
2025-08-24 21:24:27 +0200

    Update README/Add tests

--- README.md
+++ README.md
@@ -37,3 +37,45 @@ from pydantic_webhooks import get_webhook_producer
 producer = get_webhook_producer()
 producer.send_webhook(instance)
 ```
+
+**Register a custom serializer**
+
+```python
+from pydantic_webhooks import register_webhook_serializer
+from pydantic_webhooks.serializer import WebhookSerializer
+
+
+class XMLSerializer(WebhookSerializer):
+    
+    class Config(WebhookSerializer.Config):
+        format = "xml"
+        mode = "json"
+
+    def format_data(self, data: dict) -> str:
+        return dict2xml(data, wrap="webhook", indent="  ")
+
+
+register_webhook_serializer('xml', XMLSerializer)
+```
+
+**Register a custom deliverer**
+
+```python
+from pydantic_webhooks import register_webhook_deliverer
+from pydantic_webhooks.deliverer import WebhookDeliverer
+
+
+class FileDeliverer(WebhookDeliverer):
+
+    data_dir: str
+
+    class Config(WebhookDeliverer.Config):
+        formats = ["json"]
+
+    def deliver_data(self, data: str) -> None:
+        with open(os.path.join(self.data_dir, "webhook.json"), "w") as f:
+            f.write(data)
+
+
+register_webhook_deliverer('file', FileDeliverer)
+```
--- tests.py
+++ tests.py
@@ -1,14 +1,18 @@
+import os
 from contextlib import contextmanager
 from datetime import datetime
 from unittest import mock
 
 import pytest
 import requests
+from dict2xml import dict2xml
 from freezegun import freeze_time
 from pydantic import BaseModel, field_serializer, PositiveInt
 
-from pydantic_webhooks import get_webhook_producer
-from pydantic_webhooks.exceptions import WebhookDeliveryError
+from pydantic_webhooks import get_webhook_producer, register_webhook_deliverer, register_webhook_serializer
+from pydantic_webhooks.deliverer import WebhookDeliverer
+from pydantic_webhooks.exceptions import InvalidWebhookDelivererError, InvalidWebhookFormatError, WebhookDeliveryError
+from pydantic_webhooks.serializer import WebhookSerializer
 
 
 default_time = '2024-01-01T00:00:00'
@@ -33,6 +37,28 @@ class User(BaseModel):
     @field_serializer('signup_ts')
     def serialize_signup_ts(self, v: datetime | None) -> str | None:
         return v.isoformat()[:19] if v else None
+    
+
+class XMLFileDeliverer(WebhookDeliverer):
+
+    data_dir: str
+
+    class Config(WebhookDeliverer.Config):
+        formats = ["xml"]
+
+    def deliver_data(self, data: str) -> None:
+        with open(os.path.join(self.data_dir, "webhook.xml"), "w") as f:
+            f.write(data)
+
+
+class XMLSerializer(WebhookSerializer):
+    
+    class Config(WebhookSerializer.Config):
+        format = "xml"
+        mode = "json"
+
+    def format_data(self, data: dict) -> str:
+        return dict2xml(data, wrap="webhook", indent="  ")
 
 
 @pytest.fixture
@@ -52,6 +78,19 @@ def user_data():
 @pytest.fixture
 def user(user_data):
     return User(**user_data)
+
+
+@pytest.fixture
+def file_deliverer():
+    data_dir = './data'
+    os.makedirs(data_dir, exist_ok=True)
+    deliver_class = type('XMLFileDeliverer', (XMLFileDeliverer,), {'data_dir': data_dir})
+    register_webhook_deliverer('file', deliver_class)
+    register_webhook_serializer('xml', XMLSerializer)
+    yield deliver_class
+    for filename in os.listdir(data_dir):
+        os.remove(os.path.join(data_dir, filename))
+    os.rmdir(data_dir)
 
 
 def test_send_webhook(user):
@@ -96,3 +135,30 @@ def test_send_webhook_with_serialization_options(user):
         args, kwargs = mock_post.call_args
         assert 'json' in kwargs
         assert 'tastes' not in kwargs['json']
+
+
+def test_custom_components(user, file_deliverer):
+    with override_settings(DELIVERER='file', SERIALIZER='xml'):
+        producer = get_webhook_producer()
+        producer.send_webhook(user)
+        with open(os.path.join(file_deliverer.data_dir, "webhook.xml")) as f:
+            xml_data = f.read()
+        assert "<webhook>" in xml_data
+        assert "<id>1</id>" in xml_data
+        assert "<name>John Doe</name>" in xml_data
+        assert "<signup_ts>2024-01-01T00:00:00</signup_ts>" in xml_data
+        assert "<tastes>" in xml_data
+        assert "<spicy>5</spicy>" in xml_data
+        assert "<sweet>3</sweet>" in xml_data
+
+
+def test_invalid_deliverer():
+    with override_settings(DELIVERER='invalid'):
+        with pytest.raises(InvalidWebhookDelivererError):
+            get_webhook_producer()
+
+
+def test_invalid_format(file_deliverer):
+    with override_settings(DELIVERER='file'):
+        with pytest.raises(InvalidWebhookFormatError):
+            get_webhook_producer()
